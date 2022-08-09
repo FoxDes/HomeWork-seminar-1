ff7863bd4614b35de22c739510987123da4c6b71
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-09 20:48:19 +0500

    создал прогу вычисления мин и макс

--- HW1 задача2/Program.cs
+++ HW1 задача2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Напишите программу, которая на входе принимает два числа и выдает какое число больше а какое меньше
+int a = 1;
+int b = 6;
+
+int max = a;
+int min = b;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
