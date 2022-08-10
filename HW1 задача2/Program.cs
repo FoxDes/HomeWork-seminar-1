638372b2b16dd2bebf81ba1a0a80ca8b45d23bd6
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-10 14:49:13 +0500

    выполнил четвертую задачу

--- HW1 задача2/Program.cs
+++ HW1 задача2/Program.cs
@@ -47,13 +47,31 @@
 
 // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
 
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num%2 == 0)
+
+//     Console.WriteLine("Число четное");
+
+// else
+//     Console.WriteLine("Число не четное");
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
 Console.Clear();
 Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 
-if (num%2 == 0)
-
-    Console.WriteLine("Число четное");
-
-else
-    Console.WriteLine("Число не четное");
+int i = 2;
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+            Console.Write($"{i} ");
+            i += 2;
+    }
+}
+Console.WriteLine();
