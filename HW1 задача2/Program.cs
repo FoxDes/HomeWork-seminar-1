bacf369055680165ce943728e08eb59cb0948e11
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-10 13:29:13 +0500

    Выполнил задачу 3

--- HW1 задача2/Program.cs
+++ HW1 задача2/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Напишите программу, которая на входе принимает два числа и выдает какое число больше а какое меньше
+
 // Console.Write("Введите первое число: ");
 // int a = Convert.ToInt32(Console.ReadLine());
 
@@ -14,30 +15,45 @@
 // if (b < min) min = b;
 
 // Console.Write("max = ");
-// Console.WriteLine(max);
+// Console.WriteLine(max + " больше " + min);
 
 // Console.Write("min = ");
-// Console.WriteLine(min);
+// Console.WriteLine(min + " меньше " + max);
 
 
 //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
 Console.Clear();
-Console.Write("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
 
-Console.Write("Введите второе число: ");
-int c = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Число четное");
 
-
-int max = a;
-
-if (a > max) max = a;
-if (b > max) max = b;
-if (c > max) max = c;
-
-
-Console.Write("max = ");
-Console.WriteLine(max);
+else
+    Console.WriteLine("Число не четное");
